7f18f916835af00ed45c44f6cbce9b7097b04cba
syedumar-1 <syedumar4448@gmail.com>
2024-11-09 04:41:12 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slidebar from './components/Slidebar/Slidebar'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+   <>
+   <Slidebar/>
+   <Main/>
+   </>
+  )
+}
+
+export default App
