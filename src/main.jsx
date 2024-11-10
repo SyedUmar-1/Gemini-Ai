4600f9d4ff9218d2ff6426c7e4b9984d95c755c8
syedumar-1 <syedumar4448@gmail.com>
2024-11-10 10:12:47 -0800

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import ContextProvider from './context/context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ContextProvider>
+<ContextProvider>
     <App />
-  </ContextProvider>,
+ </ContextProvider>
 )
